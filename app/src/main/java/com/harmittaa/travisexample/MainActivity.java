239c372804936709356bc112be7498efed344bf3
insomnia <jutao@ihealthlabs.com.cn>
2017-07-27 16:03:04 +0800

    test folk travis

--- app/src/main/java/com/harmittaa/travisexample/MainActivity.java
+++ app/src/main/java/com/harmittaa/travisexample/MainActivity.java
@@ -2,6 +2,7 @@ package com.harmittaa.travisexample;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,6 +11,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        Log.e("test", "test");
 
     }
 }
